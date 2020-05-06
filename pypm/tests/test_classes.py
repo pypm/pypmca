a907eb4667d91b60f4d9b0223eb9bf6205db1b37
Pradeep Reddy Raamana <raamana@gmail.com>
2020-05-05 21:09:20 -0400

    rough structure for tests

---
+++ pypm/tests/test_classes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""Tests for `pypm` package."""
+
+import pytest
+
+from pypm import Model, Population, Delay, Parameter, Multiplier, Propagator, \
+    Splitter, Adder, Subtractor, Chain, Modifier, Injector
+
+
+
+def test_class_Model():
+    """tests to ensure the behaviour class Model"""
+
+
+def test_class_Population():
+    """tests to ensure the behaviour class Population"""
+
+
---
+++ pypm/tests/test_properties.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""Tests for `pypm` package."""
+
+import pytest
+
+from pypm import Model, Population, Delay, Parameter, Multiplier, Propagator, \
+    Splitter, Adder, Subtractor, Chain, Modifier, Injector
+
+
+def test_Model_properties():
+    """tests to ensure the properties of Model"""
+
+
+def test_Population_properties():
+    """tests to ensure the properties of Population"""
+
+
