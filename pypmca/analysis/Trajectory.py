ad3a5cf07e051f049f18aa974286cbc9ae475c5f
Dean Karlen <karlen@uvic.ca>
2020-06-26 11:28:45 -0700

    Do interpolation using log(alpha)

--- pypmca/analysis/Trajectory.py
+++ pypmca/analysis/Trajectory.py
@@ -4,6 +4,7 @@ Trajectory: Given a model: characterize the relation between the model transmiss
 infection trajectory growth rate (delta: fractional growth per day).
 
 This is evaluated by running the model for different values of the growth rate parameter.
+The interpolation function is done as delta(ln_a), but that is hidden from user
 
 After doing so, the following methods can be called:
 - get_delta(alpha)
@@ -26,19 +27,31 @@ class Trajectory:
         self.model = model
         self.contagious_pop_name = contagious_pop_name
         self.rate_modifier_name = rate_modifier_name
+        if rate_range is None:
+            raise TypeError('Error in constructing trajectory'+
+                            ': rate_range cannot be None')
+        if not isinstance(rate_range, list):
+            raise TypeError('Error in constructing trajectory'+
+                            ': rate_range must be a list')
+        if len(rate_range) != 2:
+            raise TypeError('Error in constructing trajectory'+
+                            ': rate_range must be a list of length 2')
+        if rate_range[0] <= 0. or rate_range[1] <= 0. or rate_range[1] <= rate_range[0]:
+            raise ValueError('Error in constructing trajectory'+
+                            ': rate_range list elements must be positive and second element larger than first')
         self.rate_range = rate_range
-        self.alphas = None
+        self.log_alphas = None
         self.deltas = None
         self.delta_inter = None
-        self.alpha_inter = None
+        self.log_alpha_inter = None
 
         self.calc_deltas()
 
     def calc_deltas(self):
-        # use 100 points for the interpolation
+        # use 100 points for the interpolation (using log alpha)
         n_point = 100
-        length = self.rate_range[1] - self.rate_range[0]
-        self.alphas = np.arange(self.rate_range[0], self.rate_range[1], length/n_point)
+        length = np.log(self.rate_range[1]) - np.log(self.rate_range[0])
+        self.log_alphas = np.arange(np.log(self.rate_range[0]), np.log(self.rate_range[1]), length/n_point)
 
         # make a copy of model, since it needs to be modified
         tr_model = copy.deepcopy(self.model)
@@ -57,7 +70,8 @@ class Trajectory:
         n_step = rate_step + 20
 
         self.deltas = []
-        for alpha in self.alphas:
+        for log_alpha in self.log_alphas:
+            alpha = np.exp(log_alpha)
             rate_par.set_value(alpha)
             tr_model.reset()
             tr_model.evolve_expectations(n_step)
@@ -65,11 +79,11 @@ class Trajectory:
             delta = ratio - 1
             self.deltas.append(delta)
 
-        self.alpha_inter = interp1d(self.deltas, self.alphas, kind='cubic')
-        self.delta_inter = interp1d(self.alphas, self.deltas, kind='cubic')
+        self.log_alpha_inter = interp1d(self.deltas, self.log_alphas, kind='cubic')
+        self.delta_inter = interp1d(self.log_alphas, self.deltas, kind='cubic')
 
     def get_delta(self, alpha):
-        return self.delta_inter(alpha).item()
+        return self.delta_inter(np.log(alpha)).item()
 
     def get_alpha(self, delta):
-        return self.alpha_inter(delta).item()
+        return np.exp(self.log_alpha_inter(delta).item())
